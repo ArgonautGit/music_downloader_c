06ef1c4b0b799f621708ad8699e547700afb2596
nick <nick-github@vansid.org>
2024-08-06 16:42:58 -0400

    Implemented commands for getting song info among other major changes

--- inc/music.h
+++ inc/music.h
@@ -2,23 +2,26 @@
 /// output in the buf string.
 /// @param command A string containing the shell command to run.
 /// @param buf The string buffer to write the output to.
-/// @param length The length of the command string (sizeof()).
 /// @return 0 if successful, -1 if failed.
-static int shell_command(char* command, char* buf, int length);
+static int shell_command(char* command, char* buf);
 
 /// @brief Takes a shell command and prints the output to stdout.
 /// Returns 0 if successful.
 /// @param command A string containing the shell command to run.
-/// @param length The string buffer to write the output to.
 /// @return 0 if successful, -1 if failed.
-static int shell_command_stdout(char* command, int length);
+static int shell_command_stdout(char* command);
 
 /// @brief Takes a URL string and downloads and extracts the audio as an mp3
 /// to the music directory.
 /// @param url The string containing the video URL.
-/// @param length The length of the char* url string.
 /// @return 0 if successful, -1 if failed.
-static int download_song(char* url, int length);
+static int download_song(char* url);
+
+/// TODO
+int get_title(char* url, char* buf);
+
+/// TODO
+int create_song(char* url);
 
 // TEST FUNCTIONS
 // TEST FUNCTIONS
--- src/music.c
+++ src/music.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX_URL_LENGTH 2048
+#define MAX_SONG_STRING_LENGTH 1024
+#define MAX_SHELL_COMMAND_LENGTH 1024
+
 typedef struct {
-  char title  [128];
-  char album  [128];
-  char artist [128];
-
+  char title  [MAX_SONG_STRING_LENGTH];
+  char album  [MAX_SONG_STRING_LENGTH];
+  char artist [MAX_SONG_STRING_LENGTH];
   int length;
-} Song; 
 
-static int shell_command_stdout(char* command, int length) {
-  if (length > 1024) {
-    fputs("Command too big!", stderr); return -1;
-  }
+  char url    [MAX_URL_LENGTH];
+
+} Song;
+
+static int shell_command_stdout(char* command) {
 
   FILE* pFile; // File stream.
   // Open pipe to shell command in read mode.
-  pFile = popen(command, "r"); 
+  pFile = popen(command, "r");
 
   // Error handling.
   if (pFile == NULL) { puts("Failed to open pipe."); return -1; }
@@ -24,40 +27,80 @@ static int shell_command_stdout(char* command, int length) {
   // While there are bytes to read, print to stdout.
   char buf[1024];
   while (fgets(buf, sizeof(buf), pFile)) fprintf(stdout, "%s", buf);
-  
+
   // Close the pipe.
   pclose(pFile);
 
   return 0;
 }
 
-static int shell_command(char* command, char* buf, int length) {
+static int shell_command(char* command, char* buf) {
 
   FILE* pFile; // File stream.
   // Open pipe to shell command in read mode.
-  pFile = popen(command, "r"); 
+  pFile = popen(command, "r");
 
   // Error handling.
   if (pFile == NULL) { puts("Failed to open pipe."); return -1; }
 
   // Write output (one line) to buf.
-  fgets(buf, length, pFile);
-  
+  fgets(buf, MAX_URL_LENGTH, pFile);
+
   // Close the pipe.
   pclose(pFile);
 
   return 0;
 }
 
-int download_song(char* url, int length) {
-  if (length > 1024) {
-    fputs("URL too big!", stderr);
-  }
+int download_song(char* url) {
 
-  char command[1024];
+  char command[MAX_URL_LENGTH];
   sprintf(command, "yt-dlp -o \"./music/%%(title)s.%%(ext)s\" --extract-audio %s", url);
 
-  shell_command_stdout(command, sizeof(command));
+  shell_command_stdout(command);
+}
+
+int get_title(char* url, char* buf) {
+
+  // Set up shell command.
+  char command[MAX_SHELL_COMMAND_LENGTH];
+  sprintf(command, "yt-dlp --quiet --no-warnings --print \"%%(title)s\" %s", url);
+
+  // Execute shell command.
+  return shell_command(command, buf);
+}
+
+int get_album(char* url, char* buf) {
+  // Set up shell command.
+  char command[MAX_SHELL_COMMAND_LENGTH];
+  sprintf(command, "ytp-dlp --quiet --no-warnings --print \"%%(album)s\" %s", url);
+
+  // Execute shell command.
+  return shell_command(command, buf);
+}
+
+int get_artist(char* url, char* buf) {
+  // Set up shell command.
+  char command[MAX_SHELL_COMMAND_LENGTH];
+  sprintf(command, "yt-dlp --quiet --no-warnings --print \"%%(channel)s\" %s", url);
+
+  // Execute shell command.
+  return shell_command(command, buf);
+}
+
+Song create_song(char* url) {
+
+  char title  [MAX_SONG_STRING_LENGTH];
+  char album  [MAX_SONG_STRING_LENGTH];
+  char artist [MAX_SONG_STRING_LENGTH];
+  int length;
+
+  get_title(url, title);
+  //get_album(url, album);
+  //get_artist(url, artist);
+  //get_length(url, length);
+
+  //get_song_info(url, song);
 }
 
 // DEBUG TEST FUNCTIONS
@@ -66,10 +109,8 @@ int download_song(char* url, int length) {
 
 int test_shell_command(void) {
   char buf[255];
-  // Test pwd.
-  printf("Testing pwd. Error code: %d, output: %s\n", shell_command("pwd", buf, sizeof(buf)), buf);
   // Test echo.
-  printf("Testing echo. Error code: %d, output: %s\n", shell_command("echo TEST", buf, sizeof(buf)), buf);
+  printf("Testing echo. Error code: %d, output: %s\n", shell_command("echo TEST", buf), buf);
   if (0 != strcmp(buf, "TEST")) return -1;
 
   return 0;
@@ -81,5 +122,12 @@ int test(void) {
 }
 
 int entry(void) {
-  download_song("https://www.youtube.com/watch?v=dQw4w9WgXcQ", 44);
+  // download_song("https://www.youtube.com/watch?v=dQw4w9WgXcQ", 44);
+
+  //Song song = create_song("test");
+  //printf("title: %s\nalbum: %s\nartist: %s\nlength: %d\nurl: %s\n", song.title, song.album, song.artist, song.length, song.url);
+
+  char buf[1024];
+  get_title("https://www.youtube.com/watch?v=dQw4w9WgXcQi", buf);
+  printf("%s", buf);
 }
